875cf3cc2a2b53a2a66890bd14172507a3ab3793
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-06-05 20:50:30 +0530

    added video to hero section

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -1,32 +1,48 @@
 "use client";
-import React from "react";
-import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
+import React, { useState } from "react";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import { useEffect } from "react";
 const Header = () => {
   const path = usePathname();
-  const {user}=useKindeBrowserClient()
+  const { user } = useKindeBrowserClient();
+  var className = "inverted";
+  var scrollTrigger = 50;
+  const [down, setdown] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolled = window.scrollY >= scrollTrigger;
+   setdown(scrolled)
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div
-      className={`fixed w-full ${
-        path === "/view" ? "bg-L-Primary" : "bg-Sur-White"
-      } h-auto max-h-[86px] md:p-[20px_60px] p-[12px] z-50 gap-0 ${
-        path === "/view" ? "border-0" : "border-b"
-      } flex justify-between items-center`}
+      id="header"
+      className={`fixed w-full  h-auto max-h-[86px] md:p-[20px_60px] p-[12px] z-50 gap-0
+       flex justify-between items-center
+       ${path==='/view'?'bg-L-Primary':down?'bg-gray-500 bg-opacity-10   backdrop-blur-sm':'bg-transparent'}
+       `}
     >
       <p
-        className={`text-3xl font-bold ${
-          path === "/view" ? "text-Sur-White" : "text-L-Primary"
-        }`}
+        className={`text-3xl font-bold text-Sur-White`}
       >
         SYC
       </p>
       <div
-        className={`w-5/12 justify-center items-center h-11 gap-5 flex  ${
-          path === "/view" ? "text-Sur-White" : "text-L-Primary"
-        }`}
+        className={`w-5/12 justify-center items-center h-11 gap-5 flex text-Sur-White`}
       >
         <button className="">
           <Link href="/">Home</Link>
@@ -35,14 +51,21 @@ const Header = () => {
         <button className="">
           <Link href="/properties">Properties</Link>
         </button>
-     
-      {user?
-    <LogoutLink className="w-fit text-center py-2 bg-Brand/Primary hover:bg-Brand/Dark px-4 rounded-full text-Sur-White">Logout</LogoutLink>:
-    <>
-     <RegisterLink className="w-fit text-center py-2 bg-Brand/Primary hover:bg-Brand/Dark px-4 rounded-full text-Sur-White">Sign up</RegisterLink>
-        <LoginLink className="w-fit text-center py-2 bg-Brand/Primary hover:bg-Brand/Dark px-4 rounded-full text-Sur-White">Sign in</LoginLink>
-    </>  
-    }
+
+        {user ? (
+          <LogoutLink className="w-fit text-center py-2 bg-Brand/Primary hover:bg-Brand/Dark px-4 rounded-full text-Sur-White">
+            Logout
+          </LogoutLink>
+        ) : (
+          <>
+            <RegisterLink className="w-fit text-center py-2 bg-Brand/Primary hover:bg-Brand/Dark px-4 rounded-full text-Sur-White">
+              Sign up
+            </RegisterLink>
+            <LoginLink className="w-fit text-center py-2 bg-Brand/Primary hover:bg-Brand/Dark px-4 rounded-full text-Sur-White">
+              Sign in
+            </LoginLink>
+          </>
+        )}
       </div>
     </div>
   );
--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -4,48 +4,51 @@ import building from "../../public/building.svg";
 import building2 from "../../public/building2.svg";
 import uk24 from "../../public/uk24.svg";
 import people from "../../public/people.svg";
-import abbey from '../../public/abbey.svg'
-import abbey2 from '../../public/lambeth.svg'
-import abbey3 from '../../public/palace.svg'
-import park from '../../public/park.svg'
+import abbey from "../../public/abbey.svg";
+import abbey2 from "../../public/lambeth.svg";
+import abbey3 from "../../public/palace.svg";
+import park from "../../public/park.svg";
 import Image from "next/image";
-import coder from '../../public/coderrrr.svg'
+
+import coder from "../../public/coderrrr.svg";
 const Hero = () => {
   return (
     <div className="">
-       <section className="w-full h-fit absolute p-16 my-8 xl:my-10 2xl:my-12 3xl:my-16 4xl:my-20">
-       <h1 className="text-[2.4rem] xl:text-[2.6rem] 2xl:text-[3.8rem] 3xl:text-[4.6rem] 4xl:text-[5.4rem] text-[#1C2B31] w-7/12 h-fit ">
-          Invest in <span className="font-semibold">real-estate</span> with
+      <section className="w-full h-fit absolute p-16 my-8 xl:my-10 2xl:my-12 3xl:my-16 4xl:my-20">
+        <h1 className="text-[2.4rem] xl:text-[2.6rem] 2xl:text-[3.8rem] 3xl:text-[4.6rem] 4xl:text-[5.4rem] text-Sur-White w-7/12 h-fit ">
+          Invest in <span className="font-semibold text-L-Primary">real-estate</span> with
           Social yield capital
         </h1>
-        <div className="flex w-4/12  justify-between">
-          <Image src={uk24} alt="" className="" />
-          <Image src={people} alt="" className="" />
-        </div>
+     
         <div className="absolute w-5/12 3xl:w-6/12  flex flex-col mt-10 xl:mt-20 2xl:mt-24 4xl:mt-48 justify-between gap-3">
-          <p className="text-[#1C2B31] text-lg xl:text-[1.5rem] 2xl:text-[1.65rem] 3xl:text-4xl 4xl:text-5xl font-medium">
+          <p className="text-Sur-White text-lg xl:text-[1.5rem] 2xl:text-[1.65rem] 3xl:text-4xl 4xl:text-5xl font-medium">
             Your gateway to smarter co-ownership and seamless property
-            management, all under the trusted guardianship of the UKs finest real
-            estate innovators.
+            management, all under the trusted guardianship of the UKs finest
+            real estate innovators.
           </p>
           <section className="w-10/12 flex gap-4 3xl:gap-8">
             <button className="w-fit bg-[#1C2B31] font-medium rounded-full p-[12px_20px] 3xl:p-[20px_28px] 4xl:p-[28px_36px] 4xl:text-xl text-Sur-White">
               Start Investing
             </button>
-            <button className="w-fit border-[#1C2B31] border rounded-full p-[12px_20px] 3xl:p-[20px_28px] 4xl:p-[28px_36px] 4xl:text-xl text-[#1C2B31] font-medium">
+            <button className="w-fit border-Sur-White border rounded-full p-[12px_20px] 3xl:p-[20px_28px] 4xl:p-[28px_36px] 4xl:text-xl text-Sur-White font-medium">
               Who are we
             </button>
           </section>
         </div>
- 
       </section>
-    <Image 
+      {/* <Image 
         src={coder} 
         alt="" 
         className=" w-full" 
         priority
-      />
-      
+      /> */}
+      <video
+        src="https://player.vimeo.com/progressive_redirect/playback/927173841/rendition/1080p/file.mp4?loc=external&amp;log_user=0&amp;signature=8773f924456bcf52f0bdd4e992db6863de1a63ec930f3a45d7fee3d5a77e745b"
+        loop
+        autoPlay
+        muted
+        className="w-full"
+      ></video>
     </div>
   );
 };
