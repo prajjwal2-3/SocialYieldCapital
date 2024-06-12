f28dd3b6245e4f52e1f91f46a046fb41ffe37a8a
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-06-12 23:25:39 +0530

    padding on hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
 
   return (
     <div className="h-full ">
-      <section className="w-full  z-40 h-fit absolute p-6 sm:p-16 my-28 sm:my-10 2xl:my-12 3xl:my-16 4xl:my-20">
+      <section className="w-full  z-40 h-fit absolute  px-[12rem] py-24  my-28 sm:my-10 2xl:my-12 3xl:my-16 4xl:my-20">
         <motion.div
           animate={{
             opacity: [0, 1],
@@ -35,9 +35,8 @@ const Hero = () => {
         
           
           </section>
-          with
-         <br/>
-          Social yield capital
+        <p> with  <span className="font-bold"> Social yield capital </span></p>
+        
         </motion.div>
        
         <motion.div
@@ -49,18 +48,18 @@ const Hero = () => {
             duration: 1,
             type: "keyframes",
           }}
-          className="absolute w-11/12 sm:w-5/12 3xl:w-6/12  flex flex-col mt-10 xl:mt-20 2xl:mt-24 4xl:mt-48 justify-between gap-3"
+          className="absolute w-11/12 sm:w-5/12 3xl:w-6/12  flex flex-col justify-between gap-3"
         >
           <p className="text-Sur-White text-lg xl:text-[1.5rem] 2xl:text-[1.65rem] 3xl:text-4xl 4xl:text-5xl font-medium">
             Your gateway to smarter co-ownership and seamless property
             management, all under the trusted guardianship of the UKs finest
             real estate innovators.
           </p>
-          <section className="w-10/12 flex gap-4 3xl:gap-8">
+          <section className="w-10/12 flex gap-4 3xl:gap-8 mt-10 xl:mt-20 2xl:mt-24 4xl:mt-48 ">
             <button className="inline-flex h-12 animate-shimmer hover:animate-shimmer2 items-center justify-center rounded-xl text-Sur-White border shadow-xl bg-[linear-gradient(110deg,#1c2b31,45%,#507B8B,55%,#1c2b31)] bg-[length:200%_100%] px-6 font-medium  transition-colors focus:outline-none focus:ring-2 focus:ring-[#94a3b8] focus:ring-offset-2 focus:ring-offset-slate-50">
               Start Investing
             </button>
-            <button className="inline-flex h-12 animate-shimmer hover:animate-shimmer2 items-center justify-center rounded-xl text-Sur-White border shadow-xl bg-[linear-gradient(110deg,#1c2b31,45%,#507B8B,55%,#1c2b31)] bg-[length:200%_100%] px-6 font-medium  transition-colors focus:outline-none focus:ring-2 focus:ring-[#94a3b8] focus:ring-offset-2 focus:ring-offset-slate-50">
+            <button className="inline-flex h-12 animate-shimmer hover:animate-shimmer2 items-center justify-center rounded-xl text-Sur-White border shadow-xl  px-6 font-medium  transition-colors focus:outline-none focus:ring-2 focus:ring-[#94a3b8] focus:ring-offset-2 focus:ring-offset-slate-50">
               Who are we
             </button>
           </section>
