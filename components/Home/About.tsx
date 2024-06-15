0624937f5b4f6ca05d346aeb0fc328c9bdb3c501
Shubham Sengar <shubhamsengar2303@gmail.com>
2024-06-15 14:29:49 +0530

    fixed the header and dashboard

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -1,49 +1,49 @@
-import React from 'react'
-import about from '../../public/about.svg'
-import about2 from '../../public/about2.svg'
-import Image from 'next/image'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
-import About2 from '../About'
-const About = () => {
-  return (
-   <>
-    <div className='py-24 px-6 md:px-14 xl:px-24 flex flex-col md:flex-row gap-10 justify-between '>
-      <section className='md:w-8/12 flex flex-col 2xl:gap-5 items-start p-3'>
-        <p className='text-xl 3xl:text-[1.5rem] 4xl:text-[2rem] font-medium text-[#507B8B]'>Who we are</p>
-        <p className='text-4xl 3xl:text-5xl 4xl:text-6xl font-semibold '>Welcome to 
-        <span className='text-4xl 3xl:text-5xl 4xl:text-6xl font-semibold text-[#507B8B] px-2'>Social Yield Capital</span>
-Simple innovation, smarter investing.</p>
-        <p className='text-[#6B7579] text-xl 4xl:text-3xl mt-5 font-medium'>At SYC, we are united by a common goal: to empower our stakeholders by providing unmatched investment opportunities. 
-Offering co-ownership in carefully selected properties, enabling accessible and lucrative investment options.</p>
-        <button className='text-[#507B8B] font-semibold mt-4 4xl:text-2xl '>Read more
-        <ArrowForwardIcon fontSize='small' className='text-[#507B8B]'/>
-        </button>
-      </section>
+// import React from 'react'
+// import about from '../../public/about.svg'
+// import about2 from '../../public/about2.svg'
+// import Image from 'next/image'
+// import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+// import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
+// import About2 from '../About'
+// const About = () => {
+//   return (
+//    <>
+//     <div className='py-24 px-6 md:px-14 xl:px-24 flex flex-col md:flex-row gap-10 justify-between '>
+//       <section className='md:w-8/12 flex flex-col 2xl:gap-5 items-start p-3'>
+//         <p className='text-xl 3xl:text-[1.5rem] 4xl:text-[2rem] font-medium text-[#507B8B]'>Who we are</p>
+//         <p className='text-4xl 3xl:text-5xl 4xl:text-6xl font-semibold '>Welcome to 
+//         <span className='text-4xl 3xl:text-5xl 4xl:text-6xl font-semibold text-[#507B8B] px-2'>Social Yield Capital</span>
+// Simple innovation, smarter investing.</p>
+//         <p className='text-[#6B7579] text-xl 4xl:text-3xl mt-5 font-medium'>At SYC, we are united by a common goal: to empower our stakeholders by providing unmatched investment opportunities. 
+// Offering co-ownership in carefully selected properties, enabling accessible and lucrative investment options.</p>
+//         <button className='text-[#507B8B] font-semibold mt-4 4xl:text-2xl '>Read more
+//         <ArrowForwardIcon fontSize='small' className='text-[#507B8B]'/>
+//         </button>
+//       </section>
       
-      <CardContainer className="inter-var">
-      <CardBody className=" relative group/card dark:border-white/[0.2] border-black/[0.1] bg-gray-50 border dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black  w-auto h-auto rounded-xl   ">
-       <CardItem className='absolute w-full'>
-       <Image src={about} alt='' className='w-full object-cover rounded-xl group-hover/card:shadow-xl '/>
-       </CardItem>
-        <CardItem translateZ="100" className="w-full  relative">
+//       <CardContainer className="inter-var">
+//       <CardBody className=" relative group/card dark:border-white/[0.2] border-black/[0.1] bg-gray-50 border dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black  w-auto h-auto rounded-xl   ">
+//        <CardItem className='absolute w-full'>
+//        <Image src={about} alt='' className='w-full object-cover rounded-xl group-hover/card:shadow-xl '/>
+//        </CardItem>
+//         <CardItem translateZ="100" className="w-full  relative">
         
-          <Image
-            src={about2}
+//           <Image
+//             src={about2}
            
-            className=" w-full object-cover rounded-xl group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
+//             className=" w-full object-cover rounded-xl group-hover/card:shadow-xl"
+//             alt="thumbnail"
+//           />
          
-        </CardItem>
+//         </CardItem>
        
-      </CardBody>
+//       </CardBody>
       
-    </CardContainer>
-    </div>
+//     </CardContainer>
+//     </div>
     
-   </>
-  )
-}
+//    </>
+//   )
+// }
 
-export default About
+// export default About
--- components/Home2.tsx
+++ components/Home2.tsx
@@ -1,133 +1,133 @@
-'use client'
-import React from 'react'
-import About from './About'
-import NumberTicker from './magicui/number-ticker'
-import { AnimatePresence, motion } from "framer-motion";
-const Home2 = () => {
-    const arr = [{
-        image:'https://framerusercontent.com/images/6snQ53S3oS3dc72M2cx8gegPU5Q.gif',
-        title:'100% Sustainable',
-        description:' Our network is 100% sustainable, and is the market leader when it comes to truly sustainable Investment services.'
-    },
-    {
-        image:'https://framerusercontent.com/images/tir56azFY9VyVXzk9TxdTIMQApE.gif',
-        title:'Budget friendly',
-        description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident natus corrupti, fugiat aliquid facere q culpa.'
-    },
-    {
-        image:'https://framerusercontent.com/images/kbtj3C4XZevzlKc8m898pwK8CU.gif',
-        title:'The Largest Network',
-        description:'The largest network across England and growing. With more than 403,470 units across England and 70% market share.'
-    },
-    {
-        image:'https://framerusercontent.com/images/H6IcKCn1tnGfoLq8FowiEFBb834.gif',
-        title:'Investor centric',
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab recusandae dolores quas est esse.'
-    },
-    {
-        image:'https://framerusercontent.com/images/0t6ZM6tCkQBKmoZriNQCO6AuOp0.gif',
-        title:'Reliable Network',
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab recusandae dolores quas est esse.'
-    },
-    {
-        image:'https://framerusercontent.com/images/0t6ZM6tCkQBKmoZriNQCO6AuOp0.gif',
-        title:'Reliable Network',
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab recusandae dolores quas est esse.'
-    },
-]
+// 'use client'
+// import React from 'react'
+// import About from './About'
+// import NumberTicker from './magicui/number-ticker'
+// import { AnimatePresence, motion } from "framer-motion";
+// const Home2 = () => {
+//     const arr = [{
+//         image:'https://framerusercontent.com/images/6snQ53S3oS3dc72M2cx8gegPU5Q.gif',
+//         title:'100% Sustainable',
+//         description:' Our network is 100% sustainable, and is the market leader when it comes to truly sustainable Investment services.'
+//     },
+//     {
+//         image:'https://framerusercontent.com/images/tir56azFY9VyVXzk9TxdTIMQApE.gif',
+//         title:'Budget friendly',
+//         description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident natus corrupti, fugiat aliquid facere q culpa.'
+//     },
+//     {
+//         image:'https://framerusercontent.com/images/kbtj3C4XZevzlKc8m898pwK8CU.gif',
+//         title:'The Largest Network',
+//         description:'The largest network across England and growing. With more than 403,470 units across England and 70% market share.'
+//     },
+//     {
+//         image:'https://framerusercontent.com/images/H6IcKCn1tnGfoLq8FowiEFBb834.gif',
+//         title:'Investor centric',
+//         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab recusandae dolores quas est esse.'
+//     },
+//     {
+//         image:'https://framerusercontent.com/images/0t6ZM6tCkQBKmoZriNQCO6AuOp0.gif',
+//         title:'Reliable Network',
+//         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab recusandae dolores quas est esse.'
+//     },
+//     {
+//         image:'https://framerusercontent.com/images/0t6ZM6tCkQBKmoZriNQCO6AuOp0.gif',
+//         title:'Reliable Network',
+//         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab recusandae dolores quas est esse.'
+//     },
+// ]
 
-  return (
+//   return (
     
-    <div className='min-h-screen pt-20 flex flex-col gap-10'>
-      <motion.div
-       animate={{
-        opacity: [0, 1],
-        y: [110, 0],
-      }}
-      transition={{
-        duration: 1,
-        type: "keyframes",
-      }}
-      className="pt-28 px-12 flex flex-col items-center justify-center text-center gap-7">
-        <p className='font-extrabold text-8xl '>Invest in <span className='text-Brand/Primary'>Real-estate</span> <br /> with Social Yield Capital</p>
-        <p className='text-2xl w-8/12'> Your gateway to smarter co-ownership and seamless property
-            management, all under the trusted guardianship of the UKs finest
-            real estate innovators.</p>
+//     <div className='min-h-screen pt-20 flex flex-col gap-10'>
+//       <motion.div
+//        animate={{
+//         opacity: [0, 1],
+//         y: [110, 0],
+//       }}
+//       transition={{
+//         duration: 1,
+//         type: "keyframes",
+//       }}
+//       className="pt-28 px-12 flex flex-col items-center justify-center text-center gap-7">
+//         <p className='font-extrabold text-8xl '>Invest in <span className='text-Brand/Primary'>Real-estate</span> <br /> with Social Yield Capital</p>
+//         <p className='text-2xl w-8/12'> Your gateway to smarter co-ownership and seamless property
+//             management, all under the trusted guardianship of the UKs finest
+//             real estate innovators.</p>
            
-        <button className='bg-L-Primary p-3 rounded-full text-white'>Start Investing</button>
-        <div className="w-11/12 mt-5">
-        <img src='https://res.cloudinary.com/dzkldv06d/image/upload/v1718376911/Morning_London_Skyline_lzz0ze.jpg' alt='' className='rounded-2xl' />
-      </div>
-      </motion.div>
-      <div className="text-center flex flex-col mt-14 px-12 gap-10">
-        <p className='text-5xl font-bold'>Why are we special</p>
+//         <button className='bg-L-Primary p-3 rounded-full text-white'>Start Investing</button>
+//         <div className="w-11/12 mt-5">
+//         <img src='https://res.cloudinary.com/dzkldv06d/image/upload/v1718376911/Morning_London_Skyline_lzz0ze.jpg' alt='' className='rounded-2xl' />
+//       </div>
+//       </motion.div>
+//       <div className="text-center flex flex-col mt-14 px-12 gap-10">
+//         <p className='text-5xl font-bold'>Why are we special</p>
 
-     <motion.div
-      initial={{ opacity: 0, y: 80 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: false, amount: 0.5 }} 
-      transition={{ duration: 0.5 }} 
-     className="flex flex-wrap justify-center items-center my-20">
-     {arr.map((e,index)=>
-     <div key={index} className="border m-4 border-black/20 rounded-3xl p-6 w-[30%] h-72 flex flex-col justify-between text-left">
-     <section className=''>
-      <img src={e.image} alt="" className='w-10 h-10' />
-     </section>
-     <section className='flex flex-col gap-4'>
-      <p className='text-2xl font-semibold'>{e.title}</p>
-      <p className='text-lg text-black/60'>
-     {e.description}
-      </p>
-     </section>
-     </div>
-    )}
-     </motion.div>
-      </div>
+//      <motion.div
+//       initial={{ opacity: 0, y: 80 }}
+//       whileInView={{ opacity: 1, y: 0 }}
+//       viewport={{ once: false, amount: 0.5 }} 
+//       transition={{ duration: 0.5 }} 
+//      className="flex flex-wrap justify-center items-center my-20">
+//      {arr.map((e,index)=>
+//      <div key={index} className="border m-4 border-black/20 rounded-3xl p-6 w-[30%] h-72 flex flex-col justify-between text-left">
+//      <section className=''>
+//       <img src={e.image} alt="" className='w-10 h-10' />
+//      </section>
+//      <section className='flex flex-col gap-4'>
+//       <p className='text-2xl font-semibold'>{e.title}</p>
+//       <p className='text-lg text-black/60'>
+//      {e.description}
+//       </p>
+//      </section>
+//      </div>
+//     )}
+//      </motion.div>
+//       </div>
    
     
-    <About/>
+//     <About/>
     
-      <div className="py-24 flex flex-col gap-6">
-        <motion.p
-          initial={{ opacity: 0, y: 80 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: false, amount: 0.5 }} 
-          transition={{ duration: 0.5 }} 
-        className='text-6xl font-extrabold text-center'>Why you should Invest with us</motion.p>
-        <motion.p 
-          initial={{ opacity: 0, y: 110 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: false, amount: 0.5 }} 
-          transition={{ duration: 0.5 }} 
-        className='text-xl text-center'>We focus on boosting our metrics rather than speaking.</motion.p>
-        <motion.section
-          initial={{ opacity: 0, y: 150 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: false, amount: 0.5 }} 
-          transition={{ duration: 0.5 }} 
-        className='flex justify-around items-center mt-10'>
-           <section className='w-3/12'>
-           <p className='text-8xl font-bold text-center'>$ <NumberTicker value={879}/>m </p>
-           <p className='text-2xl text-center'>
-           Total Investment
-           </p>
-           </section>
-           <section className='w-3/12'>
-           <p className='text-8xl font-bold text-center'> <NumberTicker value={400}/>+ </p>
-           <p className='text-2xl text-center'>
-           Properties Listed
-           </p>
-           </section>
-           <section className='w-3/12'>
-           <p className='text-8xl font-bold text-center'> <NumberTicker value={23}/>k+ </p>
-           <p className='text-2xl text-center'>
-           Investors Joined
-           </p>
-           </section>
-        </motion.section>
-      </div>
-    </div>
-  )
-}
+//       <div className="py-24 flex flex-col gap-6">
+//         <motion.p
+//           initial={{ opacity: 0, y: 80 }}
+//           whileInView={{ opacity: 1, y: 0 }}
+//           viewport={{ once: false, amount: 0.5 }} 
+//           transition={{ duration: 0.5 }} 
+//         className='text-6xl font-extrabold text-center'>Why you should Invest with us</motion.p>
+//         <motion.p 
+//           initial={{ opacity: 0, y: 110 }}
+//           whileInView={{ opacity: 1, y: 0 }}
+//           viewport={{ once: false, amount: 0.5 }} 
+//           transition={{ duration: 0.5 }} 
+//         className='text-xl text-center'>We focus on boosting our metrics rather than speaking.</motion.p>
+//         <motion.section
+//           initial={{ opacity: 0, y: 150 }}
+//           whileInView={{ opacity: 1, y: 0 }}
+//           viewport={{ once: false, amount: 0.5 }} 
+//           transition={{ duration: 0.5 }} 
+//         className='flex justify-around items-center mt-10'>
+//            <section className='w-3/12'>
+//            <p className='text-8xl font-bold text-center'>$ <NumberTicker value={879}/>m </p>
+//            <p className='text-2xl text-center'>
+//            Total Investment
+//            </p>
+//            </section>
+//            <section className='w-3/12'>
+//            <p className='text-8xl font-bold text-center'> <NumberTicker value={400}/>+ </p>
+//            <p className='text-2xl text-center'>
+//            Properties Listed
+//            </p>
+//            </section>
+//            <section className='w-3/12'>
+//            <p className='text-8xl font-bold text-center'> <NumberTicker value={23}/>k+ </p>
+//            <p className='text-2xl text-center'>
+//            Investors Joined
+//            </p>
+//            </section>
+//         </motion.section>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Home2
+// export default Home2
