b3cc016894f72add0c7af54f2a1b7ac353cf2672
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-06-20 20:30:09 +0530

    video for now

--- components/Home/Hero2.tsx
+++ components/Home/Hero2.tsx
@@ -2,13 +2,24 @@
 import React, { useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useEffect } from "react";
+import hero from '../../public/hero.svg'
+import Image from "next/image";
 export default function Hero2() {
     
   return (
     <motion.div className="min-h-screen bg-slate-600">
+       <div className="w-full bg-black/30 absolute z-20 h-screen"></div>
+      <video
+          src="https://player.vimeo.com/progressive_redirect/playback/927173841/rendition/1080p/file.mp4?loc=external&log_user=0&signature=8773f924456bcf52f0bdd4e992db6863de1a63ec930f3a45d7fee3d5a77e745b"
+          loop
+          autoPlay
+          muted
+          className="w-full h-screen absolute object-cover"
+        ></video>
+    
       <section className="absolute  h-full pt-20 px-4 w-full flex flex-col items-center justify-center overflow-x-hidden overflow-y-hidden">
         <motion.p
-          className=" h-fit w-full  text-[13rem] p-0 m-0 font-extrabold text-white/40  text-left"
+          className=" h-fit w-full  text-[13rem] p-0 m-0 font-extrabold text-white/60  text-left"
           initial={{ x: 0 }}
           animate={{ x: +1700 } }
           transition={{
@@ -21,7 +32,7 @@ export default function Hero2() {
           SOCIAL YIELD 
         </motion.p>
         <motion.p
-          className=" h-fit w-full  text-[13rem] p-0 m-0 font-extrabold text-white/40  text-right"
+          className=" h-fit w-full  text-[13rem] p-0 m-0 font-extrabold text-white/60  text-right"
           initial={{ x: 0 }}
           animate={{ x: -1700 } }
           transition={{
